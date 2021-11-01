0c1aeab8781400195d894deb4bb96558d07b98a7
Boomski99 <krisnadwipayana30@gmail.com>
2021-11-01 16:50:28 +0800

    task day-10

--- Day-10/review_message.js
+++ Day-10/review_message.js
@@ -22,4 +22,18 @@
   });
 })();
 
-function getData() {}
+function getData() {
+  //get all values
+  let fullName = document.getElementById("fullName").value;
+  let email = document.getElementById("email").value;
+  let phoneNumber = document.getElementById("phoneNumber").value;
+  let nationality = document.getElementById("nationality").value;
+  let message = document.getElementById("message").value;
+
+  console.log(message);
+  localStorage.setItem("fullName", fullName);
+  localStorage.setItem("email", email);
+  localStorage.setItem("phoneNumber", phoneNumber);
+  localStorage.setItem("nationality", nationality);
+  localStorage.setItem("message", message);
+}
