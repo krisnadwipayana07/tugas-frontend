da708e07405897f3881834b14ce2b3b29987fe98
Boomski99 <krisnadwipayana30@gmail.com>
2021-11-02 16:13:20 +0800

    task Day-11

--- frontend-alta/src/ContactUs.js
+++ frontend-alta/src/ContactUs.js
@@ -1,5 +1,98 @@
 import React from "react";
+import "./ContactUs.css";
 
 export default function ContactUs() {
-  return <div>ini contact</div>;
+  return (
+    <div>
+      <div class="col-5 background" id="bgColor">
+        <img src="asset/img/logo-ALTA-v2.png" alt="logo-ALTA" id="logo" />
+      </div>
+      <div class="col-7 p-5">
+        <form
+          action="../Day-10/review_message.html"
+          class="need-validate"
+          novalidate
+        >
+          <div class="p-5 font">
+            <h3>Contact Us</h3>
+            <div class="mb-3">
+              <label for="fullName" class="form-label"></label>
+              <b>
+                Full Name<span class="red">*</span>
+              </b>
+              <input
+                type="text"
+                class="form-control"
+                id="fullName"
+                placeholder="insert your fullname here"
+                required
+              />
+              <div class="invalid-feedback">Full Name cannot be empty</div>
+            </div>
+            <div class="mb-3">
+              <label for="email" class="form-label">
+                <b>
+                  Email Address<span class="red">*</span>
+                </b>
+              </label>
+              <input
+                type="email"
+                class="form-control"
+                id="email"
+                placeholder="example@domain.com"
+                required
+              />
+              <div class="invalid-feedback">Email Address cannot be empty</div>
+            </div>
+            <div class="mb-3">
+              <label for="phoneNumber" class="form-label">
+                <b>
+                  Phone Number<span class="red">*</span>
+                </b>
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="phoneNumber"
+                placeholder="087278XXXXXXXX"
+                required
+              />
+              <div class="invalid-feedback">Phone number cannot be empty</div>
+            </div>
+            <div class="mb-3">
+              <label for="nationality" class="form-label">
+                <b>Nationality</b>
+              </label>
+              <select
+                class="form-select"
+                aria-label="Default select example"
+                id="nationality"
+              >
+                <option selected value="indonesia">
+                  Indonesia
+                </option>
+                <option value="inggris">Inggris</option>
+                <option value="korea">Korea</option>
+                <option value="jepang">Jepang</option>
+              </select>
+            </div>
+            <div class="mb-3">
+              <label for="message" class="form-label">
+                {" "}
+                <b>Message</b>{" "}
+              </label>
+              <textarea class="form-control" id="message" rows="3"></textarea>
+            </div>
+            <button
+              type="submit"
+              class="btn-orange border p-2 px-4"
+              onclick="getData()"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
--- frontend-alta/src/Homepage.js
+++ frontend-alta/src/Homepage.js
@@ -4,10 +4,10 @@ import "./Homepage.css";
 
 export default function Homepage() {
   return (
-    <div>
+    <div className="bgImage">
       <Navbar />
-      <div class="bgImage p-5">
-        <div class="p-5 gathering">
+      <div className="p-5">
+        <div className="p-5 gathering">
           <img
             src="asset/img/matthew-hamilton-tNCH0sKSZbA-unsplash.jpg"
             alt="profile-pic"
@@ -15,13 +15,14 @@ export default function Homepage() {
             height="350px"
             className="border"
           />
-          <div class="m-5 font500">
+          <div className="m-5 ms-4 font500">
             <h4>Hi, my name is</h4>
             <h1 className="fontSetting">Anne Sullivan</h1>
-            <h3 class="my-4">I build things for the web</h3>
+            <h3 className="my-4">I build things for the web</h3>
             <button
-              type="button"
-              class="btn bgOrange text-white px-4 gambarPic border"
+              type="submit"
+              class="btn-orange border p-2 px-4"
+              onclick="location.href='/contactus'"
             >
               Get in Touch
             </button>
